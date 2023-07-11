12c6859dd46843f4e89075479163b25ecfc5f642
Nicolas Lopez <niclopezt@gmail.com>
2023-07-11 14:23:15 +1000

    Added explanatory comments to the index.js code

--- main.js
+++ main.js
@@ -1,69 +1,101 @@
 function getComputerChoice() {
     /**
-     * Randomly, return one of the three possible values.
+     * Randomly, return one of the three possible values between Rock, Paper 
+     * and Scissors.
      * This result represent the computer selection.
      */
 
-    const choices = ['rock', 'paper', 'scissors'];
+    const choices = ["rock", "paper", "scissors"];
 
-    return choices[(Math.floor(Math.random() * 3))];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
 function capitalize(str) {
     /**
-     * Capitalize a String (str). Return a new string which its first word is 
-     * capitalized and the rest of the initial string is concatenated as lowercase.
+     * Capitalize a String (str). Return a new string which its first word is
+     * capitalized and the rest of the initial string is concatenated as 
+     * lowercase.
+     * It's included only for printing and formatting purposes.
      */
     return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 
 function playRound(userChoice, computerChoice) {
     /**
+     * Receive two different string parameters.
      * Return a String according with the input selection userChoice (string)
-     * and computerChoice (string), following the classic rules of the Rock 
+     * and computerChoice (string), following the classic rules of the Rock
      * Paper Scissors game.
      */
 
-    const tie = 'It\'s a tie';
-    const youWin = 'You win! ' + capitalize(userChoice) + ' beats ' + capitalize(computerChoice);
-    const youLose = 'You lose! ' + capitalize(computerChoice) + ' beats ' + capitalize(userChoice);
+    // The possible results are saved in three variables (string type)
+    const tie = "It's a tie";
+    const youWin =
+        "You win! " +
+        capitalize(userChoice) +
+        " beats " +
+        capitalize(computerChoice);
+    const youLose =
+        "You lose! " +
+        capitalize(computerChoice) +
+        " beats " +
+        capitalize(userChoice);
 
+    // The comparison is made parsing the userChoice to lowercase.
+    // The tie and wining case scenarios are evaluated to return the respective
+    // result message, otherwise (including different random user inputs) return
+    // the youLose message.
     if (userChoice.toLowerCase() == computerChoice) {
         return tie;
     }
     if (
-        (userChoice.toLowerCase() == 'rock' && computerChoice == 'scissors') ||
-        (userChoice.toLowerCase() == 'paper' && computerChoice == 'rock') ||
-        (userChoice.toLowerCase() == 'scissors' && computerChoice == 'paper')
+        (userChoice.toLowerCase() == "rock" && computerChoice == "scissors") ||
+        (userChoice.toLowerCase() == "paper" && computerChoice == "rock") ||
+        (userChoice.toLowerCase() == "scissors" && computerChoice == "paper")
     ) {
         return youWin;
     }
-    
+
     return youLose;
 }
- 
+
 function game() {
     /**
      * Run a loop, in which a round is played, the needed times til userScore or
      * computerScore reach a value of 3.
      */
 
+    // Maximum score to reach until get a winner
+    let maxWinningRounds = 3;
+
+    // Numeric variables to keep record of the computer and user score.
     let userScore = 0;
     let computerScore = 0;
-    let userChoice = '';
-    let resultRound = '';
+
+    // String variable to receive the user input via prompt().
+    let userChoice = "";
+
+    // String variable to save the result of the playRound function.
+    let resultRound = "";
+
 
     while (userScore < 3 && computerScore < 3) {
-        userChoice = prompt('Select your movement: ');
+        userChoice = prompt("Select your movement: ");
         resultRound = playRound(userChoice, getComputerChoice());
         console.log(resultRound);
 
-        if (resultRound[4] == 'w') {userScore++;}
-        if (resultRound[4] == 'l') {computerScore++;}
+    // Check the fourth char of the string to determine who won the round and
+    // add one point to the respective score counter.
+        if (resultRound[4] == "w") {
+            userScore++;
+        }
+        if (resultRound[4] == "l") {
+            computerScore++;
+        }
     }
 
-    console.log('User: ' + userScore);
-    console.log('PC: ' + computerScore);
+    console.log("User: " + userScore);
+    console.log("PC: " + computerScore);
 }
 
 game();
